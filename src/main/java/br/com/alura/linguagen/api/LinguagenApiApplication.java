dd4268782e061bab2992fc536490ab6b0d0ae0e2
Janderson <janderson.sampaio@geosales.com.br>
2023-03-31 23:35:08 -0300

    Subindo o projeto inicial

---
+++ src/main/java/br/com/alura/linguagen/api/LinguagenApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.linguagen.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinguagenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinguagenApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/linguagen/api/LinguagenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.linguagen.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinguagenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
