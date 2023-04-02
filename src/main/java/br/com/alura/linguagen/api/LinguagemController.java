473c0b9197edfe7089e49e95c9b9169628d09d43
Janderson <janderson.sampaio@geosales.com.br>
2023-04-02 15:22:58 -0300

    Subindo o DELETE

--- src/main/java/br/com/alura/linguagen/api/LinguagemController.java
+++ src/main/java/br/com/alura/linguagen/api/LinguagemController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,12 +28,11 @@ public class LinguagemController {
     public Linguagem cadastrarLinguagem(@RequestBody Linguagem linguagem) {
         Linguagem linguagemSalva = repositorio.save(linguagem);
         return linguagemSalva;
-/*
-    @DeleteMapping("/linguagens")
-    linguagem.delete(linguagem);*/
+    }
 
-    
-
+    @DeleteMapping("/linguagens/{id}")
+    public void excluirLinguagem(@PathVariable String id) {
+         repositorio.deleteById(id);
     }
 
     }
